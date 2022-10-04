e9ab9f1c479db04fe7238c5a22a360d35aac4ae2
oleksandr-vt <vintoniakoleksandr12@gmail.com>
2022-10-04 11:28:17 +0300

    initial commit

---
+++ src/views/ListView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In consectetur numquam saepe! Molestiae vero ab
+        exercitationem necessitatibus quisquam assumenda corporis officia rerum, minima animi veritatis iusto quo!
+        Obcaecati, nostrum ipsum?</p>
+</template>
